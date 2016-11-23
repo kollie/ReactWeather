5813cf1ea18ac55480e634d3f282fa1a4c3ab441
Israel Z. Kollie <zikollie@gmail.com>
2016-11-23 11:26:26 +0000

    Update about page

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -4,8 +4,22 @@ var About = (props) => {
   return (
 
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page of my React Weather app, hope it's super exciting for you.</p>
+      <h1 className="text-center">About</h1>
+      <p>This is my first application using React. This applicatin basically lets
+        you type the name of any city and get the temperature. I'm'
+        actually getting these skills from the Complete React Web Developer Course offered on Udemy.
+      </p>
+      <p>
+        You can access some of the tools I used in this application below:
+      </p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - A JavaScript framework developed by facebook.
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> - I used Open Weather Map to quickly return the temperature of the city you're looking for.
+        </li>
+      </ul>
     </div>
   )
 };
