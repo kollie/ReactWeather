7e2daaf90077429eb4bfffcddb7d87f709eb5d94
Israel Z. Kollie <zikollie@gmail.com>
2016-11-23 11:00:20 +0000

    Update with Grid system

--- app/components/examples.jsx
+++ app/components/examples.jsx
@@ -1,11 +1,20 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to my React app!!!</p>
+      <h1 className="text-center">Examples</h1>
+      <p>You can try it out with these few locations:</p>
+      <ol>
+        <li>
+          <Link to='/?location=Monrovia'>Monrovia, LR</Link>
+        </li>
+        <li>
+          <Link to='/?location=Lagos'>Lagos, NG</Link>
+        </li>
+      </ol>
     </div>
   )
 };
